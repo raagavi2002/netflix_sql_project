98b9710d3991744d72013e0a97f9bcf6a69277c4
raagavi2002 <111857913+raagavi2002@users.noreply.github.com>
2024-12-20 16:16:19 +0530

    Add files via upload

---
+++ Schemas Of Netflix.sql
@@ -0,0 +1,16 @@
+----Netflix Project
+
+Create table Netflix
+(
+show_id varchar(6),
+type varchar(10),	
+title varchar(150),	
+director varchar(210),	
+casts varchar(1000),	
+country varchar(150),	
+date_added varchar(50),	
+release_year int,	
+rating varchar(10),	
+duration varchar(20),	
+listed_in varchar(125),	
+description varchar(300));
